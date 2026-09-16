d4a0156484641d49fbd88abd71873361ebe56002
andrewestbrook <drewestbrook07@gmail.com>
2026-09-16 16:55:46 +0000

    chore: created is_prime function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::io;
 
 fn main() {
     println!("{}", is_even(4));
+
+    println!("{}", is_prime(5));
 }
 
 // C++ == bool is_even(int number)
@@ -16,3 +18,10 @@ fn is_even(number: i32) -> bool {
 	}
 }
 
+fn is_prime(number: i32) -> bool {
+	if number % 2 != 0 {
+		true
+	} else {
+		return false;
+	}
+}
