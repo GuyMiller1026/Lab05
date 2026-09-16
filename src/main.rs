26c65366f7c6c20017c39bbcf0bbcd1a55c1ba05
Guy <guyben1026@gmail.com>
2026-09-16 16:44:16 +0000

    feat: completed first function for lab 05

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", is_even(4));
 }
+
+// C++ == bool is_even(int number)
+// Python == def is_even(number)
+fn is_even(number: i32) -> bool {
+	if number % 2 == 0 {
+                // Use the return keyword -- needs ;
+		// return true;
+                true // Put the return value or variable, no ;
+	} else {
+		return false;
+	}
+}
+
